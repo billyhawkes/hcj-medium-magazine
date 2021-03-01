04cb3d5add601eb44637dfe0eaa2f43e72ddcc97
Billy Hawkes <billyhawkes02@gmail.com>
2021-03-01 15:46:05 -0500

    Added Carousel

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-let socials = document.querySelector(".socials");
+// Carousel
+const carouselImages = document.querySelector(".carouselimages");
+const carouselButtons = document.querySelectorAll(".carouselbutton");
+let carouselXpos = 0;
+let imageNum = 0;
+
+for (let button of carouselButtons) {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (e.target.classList.contains("last") && imageNum > 0) {
+            imageNum--;
+            carouselXpos += 1200;
+        }
+        if (e.target.classList.contains("next") && imageNum < 4) {
+            imageNum++;
+            carouselXpos -= 1200;
+        }
+        carouselImages.style.transform = `translateX(${carouselXpos}px)`;
+    });
+}
